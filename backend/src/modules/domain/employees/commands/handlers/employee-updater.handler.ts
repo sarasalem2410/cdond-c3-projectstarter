471bd41ca4d6356c94137cfe6b606dbb7362db26
sarasalem2410 <63931495+sarasalem2410@users.noreply.github.com>
2021-04-20 23:30:00 +0200

    Update employee-updater.handler.ts

--- backend/src/modules/domain/employees/commands/handlers/employee-updater.handler.ts
+++ backend/src/modules/domain/employees/commands/handlers/employee-updater.handler.ts
@@ -47,7 +47,7 @@ export class EmployeeUpdater extends BaseCommandHandler<UpdateEmployee, void> {
     employee.displayName = displayName;
     employee.companyEmail = companyEmail;
     employee.personalEmail = personalEmail;
-    employee.birthdate = moment(birthdate)
+    employee.birthdate = new Date(moment(birthdate))
       .utc()
       .format();
     employee.address = address;
@@ -58,7 +58,7 @@ export class EmployeeUpdater extends BaseCommandHandler<UpdateEmployee, void> {
     employee.city = city;
     employee.salary = +salary;
     employee.salaryType = SalaryType[salaryTypeKey];
-    employee.effectiveDate = moment(effectiveDate)
+    employee.effectiveDate = new Date(moment(effectiveDate))
       .utc()
       .format();
 
